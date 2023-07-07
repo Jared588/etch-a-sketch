bade5f8bffdfa54736b73bc1fcaad1c530bf3d52
Jared588 <jaredeprice@gmail.com>
2023-07-07 16:45:53 +0200

    Add basic foundational code
    Add the basic foundation to build and corect upon

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the grid element
+    const sketchpad = document.getElementById("sketchpad");
+    let gridSize = 16;
+    let totalSquares = gridSize * gridSize;
+  
+    // Configure each square and place within the grid
+    let squareWidth = Math.floor(sketchpad.offsetWidth / gridSize);
+    let squareHeight = Math.floor(sketchpad.offsetHeight / gridSize);
+  
+    // Set grid-template-columns property to create a grid layout
+    sketchpad.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  
+    // Add event listener to each square for drawing functionality
+    for (let i = 0; i < totalSquares; i++) {
+      const square = document.createElement('div');
+      square.style.width = squareWidth + 'px';
+      square.style.height = squareHeight + 'px';
+  
+      square.addEventListener('mouseover', function() {
+        // Set the background color of the square when the mouse is over it
+        square.style.backgroundColor = 'black';
+      });
+  
+      sketchpad.appendChild(square);
+    }
+  });
