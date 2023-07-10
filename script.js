ccbe0466531d3dd18f17fd7311d028eccb15ca96
Jared588 <jaredeprice@gmail.com>
2023-07-10 20:24:52 +0200

    Fix sketchpad functionality

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Get the grid element
     const sketchpad = document.getElementById("sketchpad");
-    let gridSize = 16;
+    let gridSize = 64;
     let totalSquares = gridSize * gridSize;
   
     // Configure each square and place within the grid
     let squareWidth = (sketchpad.offsetWidth / gridSize);
     let squareHeight = (sketchpad.offsetHeight / gridSize);
-  
-    // Set grid-template-columns property to create a grid layout
-    sketchpad.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   
     // Add event listener to each square for drawing functionality
     for (let i = 0; i < totalSquares; i++) {
@@ -17,11 +14,40 @@ document.addEventListener('DOMContentLoaded', function() {
       square.style.width = squareWidth + 'px';
       square.style.height = squareHeight + 'px';
   
-      square.addEventListener('mouseover', function() {
-        // Set the background color of the square when the mouse is over it
-        square.style.backgroundColor = 'black';
+      square.addEventListener('mousedown', function() {
+        // Set the background color of the square when the mouse is pressed
+        startDrawing(square);
       });
-  
+
       sketchpad.appendChild(square);
     }
+
+    document.addEventListener('mouseup', stopDrawing);
+    document.addEventListener('dragstart', function(event) {
+      event.preventDefault(); // Disable default dragging behavior
+    });
   });
+
+var isDrawing = false;
+
+// drawing functions
+function startDrawing (square) {
+  isDrawing = true;
+  square.style.backgroundColor = 'black';
+  square.addEventListener('mouseenter', draw);
+  document.addEventListener('mousemove', draw);
+}
+
+function stopDrawing () {
+  isDrawing = false;
+  document.removeEventListener('mousemove', draw);
+}
+
+function draw(event) {
+  if (isDrawing === true) {
+    const square = event.target;
+    if (square.matches('#sketchpad > div')) {
+      square.style.backgroundColor = 'black';
+    }
+  }
+}
